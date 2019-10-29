d90d749f417f665c074a98cc9510348f06a1f54f
Theerathat Pornprinya <theerathat2008@dyn3181-73.wlan.ic.ac.uk>
2019-10-29 13:47:25 +0000

    [tp416] Fix withdraw function to work and initialises the work for daily auction

--- src/Smart_Contract/build/contracts/AggregrateDemandBid.sol
+++ src/Smart_Contract/build/contracts/AggregrateDemandBid.sol
@@ -1,4 +1,4 @@
-pragma solidity 0.5.3;
+pragma solidity 0.5.11;
 
 contract AggregrateDemandBid {
   //Beneficiary is the account that holds all the bids
@@ -11,8 +11,16 @@ contract AggregrateDemandBid {
   //By default initialized to false
   bool public closed;
 
+  address payable[] public players;
+
   //storing prediction amount corresponding to an address
   mapping (address => Agent_Details) agent_predictions;
+
+  mapping (uint => mapping (address => Agent_Details)) agent_predictions_daily;
+
+  //incrementor starting from when initializing the contract
+  uint day_increment = 1;
+
   //number of bets
   uint8 private agentCount;
   uint private highestInterval;
@@ -40,26 +48,36 @@ contract AggregrateDemandBid {
   // Log the event about a bet submission being made by an address and its prediction
   event BetSubmission(address indexed accountAddress, uint prediction);
   // Calculate the shares that agent won
-  event AuctionEnded(uint amount);
+  event AuctionEnded(uint day_number, uint amount);
 
   //event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
-  /*
-  //calculates the money gain after settement value is found
-  //if an agent wins, they get double the amount of the bet placed
-  function payout() public payable {
-      if (game.agent.guess == settlementValue) {
-        game.agent.address.transfer(game.betAmount * 2);
-      }
-  }*/
 
+
+  //Only allowed to play if he didn't already play
   //Bid on the auction with the value send together with this transaction
-  function submitBet(uint amount) public payable {
+  function submitBet(uint _guess) public payable {
+
+
+
     //No arguments are necessary, all
+
+    //Check if the player already exist
+    require(!checkPlayerExists(msg.sender));
+
     //Revert the call if the bidding period is over.
     require (now <= auctionEnd, "Auction already ended.");
 
-    agent_predictions[msg.sender].guess =  amount;
+    //rejct payment of 0 ether
+    require (msg.value > 0);
+
+
+
+    //set agent predictionss
+    agent_predictions[msg.sender].guess =  _guess;
+
+    //add the adress of the player to the players array
+    players.push(msg.sender);
 
     moneyInTheContract += msg.value;
 
@@ -72,14 +90,26 @@ contract AggregrateDemandBid {
   //Agent could withdraw the bid at the end of the day
   function withdraw() public returns (bool) {
 
+
+
+    //the bet still has time left
+    if (now < auctionEnd) return false;
+
+
     if (agent_predictions[msg.sender].guess >= lowestInterval &&
     agent_predictions[msg.sender].guess <= highestInterval &&
     !agent_predictions[msg.sender].claimed) {
         // &&closed
-      msg.sender.send(10**18);
+      (msg.sender).transfer(2 ether);
       agent_predictions[msg.sender].claimed = true;
       return true;
     }
+
+
+
+
+
+
     /*uint amount = agent_predictions[msg.sender].reward;
     if (amount > 0) {
       agent_predictions[msg.sender].reward = 0;
@@ -89,6 +119,11 @@ contract AggregrateDemandBid {
 
     }*/
     return false;
+  }
+
+  //return the day that the auction is in right now
+  function getDayCount() public view returns (uint) {
+      return day_increment;
   }
 
   //End the auction and calculates which agent gets the rewards
@@ -102,7 +137,7 @@ contract AggregrateDemandBid {
     //uint money_share = findMoneyShare();
     setSettlementValue();
 
-    emit AuctionEnded(2);
+    emit AuctionEnded(day_increment, 2);
   }
 
   function findHighestInterval() private {
@@ -121,7 +156,7 @@ contract AggregrateDemandBid {
     return getPot() / findNumberOfAgentWithinInterval();
   }
 
-  function getPot() public returns (uint) {
+  function getPot() public view returns (uint) {
       return moneyInTheContract;
   }
 
@@ -131,5 +166,19 @@ contract AggregrateDemandBid {
     settlementValue = 4200;
     findLowestInterval();
     findHighestInterval();
+  }
+
+  //Check if a player is exist or not
+  //If the players is found in the players array, then true is returned
+  //False return if noone is found
+  function checkPlayerExists(address player) public view returns (bool) {
+    for (uint256 i = 0; i < players.length; i++) {
+      if (players[i] == player) return true;
+    }
+    return false;
+  }
+
+  function resetDaily() private {
+
   }
 }
