7aa2b89af478d30216faa75d75c01efb5f364fe0
Jeep Thepro <jeepsuphanat@gmail.com>
2020-01-10 10:35:30 +0000

    Cleaned up agent code, removed redundant code

--- agent/scripts/agent.py
+++ agent/scripts/agent.py
@@ -2,48 +2,56 @@
 import time
 from brownie import accounts, convert, DemandBid
 from threading import Thread
+import struct
+import random
 
 import requests
 
+colour = "\033[1;30m"
+
 class Agent(Thread):
 
-    def __init__(self, num, password,  ec2_dns, daytime, rounds):
+    def __init__(self, num, password, daytime, rounds):
         Thread.__init__(self)
         self.num = num
-        self.ec2_dns = ec2_dns
         self.password = password
         self.daytime = daytime
         self.rounds = rounds
         self.prediction = None
+        self.i = 0
 
     def get_prediction(self):
-        # if self.num == 1:
-        #     headers = {
-        #         'Content-Type': 'application/json',
-        #     }
-        #     data = '{"dataset_size": "100"}'
-        #     response = requests.post(ec2_dns, headers=headers, data=data)
-        #     self.prediction = int(response.content)
-        # else:
-        self.prediction = 4100
+        if self.num == 1:
+            headers = {
+                'Content-Type': 'application/json',
+            }
+            data = '{"prediction_size": "10"}'
+            response = requests.post('http://ec2-18-188-39-149.us-east-2.compute.amazonaws.com:80/predict', headers=headers, data=data)
+            values = response.content.split()
+            self.prediction = []
+            for v in values:
+                v = int(float(str(v, 'utf-8')) * 1000000)
+                self.prediction.append(v)
+        else:
+            self.prediction = [2000000] * 10
 
-    def submit_bet(self, amount):
-        bet = bytes(DemandBid[0].returnKeccak256OfEncoded(self.prediction, self.password))
+    def submit_bet(self, n, amount):
+        bet = bytes(DemandBid[0].returnKeccak256OfEncoded(self.prediction[n], self.password))
         eth_amount = '{0} ether'.format(amount)
         DemandBid[0].submitBet.transact(bet, {'value':eth_amount,'from':accounts[self.num]})
 
-        print('Agent {0} submitted a bet of {1} for for prediction {2} on day {3}'.format(
-            self.num, eth_amount, self.prediction, DemandBid[0].getCurrentDay()
+        print(colour,'Agent {0} submitted a bet of {1} for for prediction {2} on day {3}'.format(
+            self.num, eth_amount, self.prediction[n], DemandBid[0].getCurrentDay()
         ))
 
-    def reveal_bet(self):
-        DemandBid[0].revealBet(self.prediction, self.password, {'from': accounts[self.num]})
+    def reveal_bet(self, n):
+        DemandBid[0].revealBet(self.prediction[n], self.password, {'from': accounts[self.num]})
 
-        print('Agent {0} revealed their bet on day {1}'.format(self.num, DemandBid[0].getCurrentDay()))
+        print(colour,'Agent {0} revealed their bet on day {1}'.format(self.num, DemandBid[0].getCurrentDay()))
 
     def calculate_reward(self):
         DemandBid[0].calculateReward({'from': accounts[self.num]})
-        print('Agent {0} calculated reward on day {1}'.format(
+        print(colour,'Agent {0} calculated reward on day {1}'.format(
             self.num, DemandBid[0].getCurrentDay()
         ))
 
@@ -51,33 +59,51 @@ class Agent(Thread):
         reward = DemandBid[0].getRewardAmount(day, {'from': accounts[self.num]})
         DemandBid[0].withdraw({'from': accounts[self.num]})
 
-        print('Agent {0} received a reward of {1} ether on day {2}'.format(
-            self.num, reward, DemandBid[0].getCurrentDay()
+        print(colour,'Agent {0} received their reward on day {1}'.format(
+            self.num, DemandBid[0].getCurrentDay()
         ))
         # print('Agent {0} has balance {1}'.format(self.num, accounts[self.num].balance()))
 
     def run(self):
         day = 0
+        self.get_prediction()
+        # Day 0
+        self.submit_bet(self.i, 5)
+
+        time.sleep(self.daytime*12/24)
+        self.reveal_bet(self.i)
+        time.sleep(self.daytime*12/24)
+
+        self.i += 1
+        day += 1
+        self.rounds -= 1
+
+        # Day 1
+        self.submit_bet(self.i, 5)
+        time.sleep(self.daytime*12/24)
+        self.reveal_bet(self.i)
+        time.sleep(self.daytime*12/24)
+
+        self.i += 1
+        day += 1
+        self.rounds -= 1
+
         while self.rounds:
-            # Day 0
-            self.get_prediction()
-            self.submit_bet(5)
-
-            # time.sleep(self.daytime*23/24)
-            self.reveal_bet()
-            time.sleep(self.daytime)
-
-            # Day 1
-            time.sleep(self.daytime)
 
             #Day 2
+            time.sleep(self.daytime/24)
             self.calculate_reward()
-            time.sleep(self.daytime*4/24)
+            print()
+            self.submit_bet(self.i, 5)
+            time.sleep(self.daytime*8/24)
             self.receive_reward(day)
-            time.sleep(self.daytime*20/24)
+            time.sleep(self.daytime*3/24)
+            self.reveal_bet(self.i)
+            time.sleep(self.daytime*12/24)
 
-            day += 3
+            day += 1
             self.rounds -= 1
+            self.i += 1
 
 
 class Oracle(Agent):
@@ -95,13 +121,13 @@ class Oracle(Agent):
     def set_settlement(self, settlement):
         DemandBid[0].setSettlementValue(settlement, {'from': accounts[0]})
 
-        print('The oracle has set the settlement value at: {0}'.format(settlement))
+        print(colour,'The oracle has set the settlement value at: {0}'.format(settlement))
 
     def run(self):
         time.sleep(self.daytime*2)
         while self.rounds and self.i < len(self.settlements):
             self.set_settlement(self.settlements[self.i])
-            print(DemandBid[0].getSettlementValue(0, {'from': accounts[0]}))
+            time.sleep(self.daytime)
             self.i += 1
             self.rounds -= 1
             time.sleep(self.daytime)
--- agent/scripts/demo.py
+++ agent/scripts/demo.py
@@ -2,19 +2,17 @@ from brownie import accounts, DemandBid
 from .agent import Agent, Oracle
 
 def main():
-    daytime = 10
+    daytime = 20
     oracle = None
     agents = []
     rounds = 10
-    settlements = [4100] * 10
+    settlements = [101643, 99419, 100924, 92123, 98228, 96530, 97947, 96135, 94158, 97995]
 
     for i in range(6):
         if i == 0:
             oracle = Oracle(0, "oracle", daytime, rounds, settlements)
-        # elif i == 1:
-        #     agents.append(Agent(i, "agent{0}".format(i), "", daytime, rounds))
         else:
-            agents.append(Agent(i, "agent{0}".format(i), "", daytime, rounds))
+            agents.append(Agent(i, "agent{0}".format(i), daytime, rounds))
 
     accounts[0].deploy(DemandBid, daytime)
     oracle.start()
