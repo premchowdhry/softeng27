798b2095cb3f758f55d1ef5d54eb678065034f7c
Jeep Thepro <jeepsuphanat@gmail.com>
2020-01-10 11:46:18 +0000

    Removed redundant files

--- reward2.py
+++
@@ -1,5 +0,0 @@
-from brownie import *
-p = project.load('token', name="TokenProject")
-p.load_config()
-from brownie.project.TokenProject import *
-network.connect('development')
--- src/migrations/2_deploy_demandBid.js
+++
@@ -1,5 +0,0 @@
-var DemandBid = artifacts.require("DemandBid");
-
-module.exports = function(deployer) {
-  deployer.deploy(DemandBid, 9999);
-};
